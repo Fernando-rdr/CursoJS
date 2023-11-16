3967aa9e5755aa2853b32795473fa3e9d8eeefbc
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-11-16 21:00:43 +0100

    accomplished challenge 2 js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,21 @@
-alert('ola')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    /* var hora = 6 */
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+       // Bom dia fdb467
+       img.src = 'fotomanha.png'
+       document.body.style.background = '#fdb467'
+    } else if (hora >= 12 && hora <= 18) {
+        // boa tarde 828e93
+        img.src = 'fototarde.png'
+        document.body.style.background = '#828e93'
+    } else {
+        // boa noite 4c3853
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#4c3853'
+    }
+}
