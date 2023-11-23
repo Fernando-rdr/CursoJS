9aaeac96463103aa9e573e552e6faf82a8860705
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-11-23 16:47:56 +0100

    ex 14 javaScript

---
+++ aula14/ambiente.js
@@ -0,0 +1,24 @@
+console.log("inicio")
+for(var c = 1;c <= 4;c++) {
+    console.log(c)
+}
+console.log("FIM")
+
+
+
+
+/*
+for (var c = 1; c <= 3;c++) {
+    console.log(c)
+}
+*/
+
+
+
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c <= 5)
+*/
