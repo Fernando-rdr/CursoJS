2cbd9ca76fbc5c9404a1f666d57fd9aca7c51656
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-11-25 18:47:53 +0100

    practicing js ex 16

--- aula14ex/modelo/script.js
+++ aula14ex/modelo/script.js
@@ -1,1 +1,13 @@
-alert('ola')
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por Favor, Digite um numero!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+        }
+    }
+}
