9833962f8786bf2208ec10c1927fbbb24e12faa8
Fernando-rdr <150026790+Fernando-rdr@users.noreply.github.com>
2023-11-26 23:36:32 +0100

    array js

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5,2,8,9]
+num[4] = 1
+num.push(3)
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+console.log(`Array em ordem: ${num.sort()}`)
